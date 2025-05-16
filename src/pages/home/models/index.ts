4b1f2f5402042fb4b922c82abb40b7954493614b
Michael <michael.sousa@terceirizados.farmarcas.com.br>
2025-05-15 23:12:23 -0300

    feat: melhoria em tipagem

--- src/pages/home/models/index.ts
+++ src/pages/home/models/index.ts
@@ -1,10 +1,58 @@
 import type { IPagination } from 'src/@types';
 
-export interface IStateHome {
-  loadingPage: boolean;
-  listCharacteres: ICharacter[];
-  listDetails: IDetails;
-  pagination: IPagination;
+// Enums devem vir antes das interfaces que os utilizam
+export enum GenderName {
+  male = 'Masculino',
+  female = 'Feminino',
+  unknown = 'Desconhecido',
+  genderless = 'Sem gênero',
+}
+
+export enum GenderColor {
+  male = 'grey-8',
+  female = 'purple',
+  unknown = 'green',
+  genderless = 'orange',
+}
+
+export enum StatusName {
+  dead = 'Morto',
+  alive = 'Vivo',
+  unknown = 'Desconhecido',
+}
+
+export enum StatusColor {
+  dead = 'red',
+  alive = 'green',
+  unknown = 'grey-8',
+}
+
+export enum SpeciesName {
+  alien = 'Alienígena',
+  human = 'Humano',
+  unknown = 'Desconhecido',
+  humanoid = 'Humanóide',
+  animal = 'Animal',
+  robot = 'Robô',
+  poopybutthole = 'Poopybutthole',
+  mythological = 'Mitológico',
+  cronenberg = 'Cronenberg',
+  disease = 'Doença',
+  planet = 'Planeta',
+}
+
+export interface ILocation {
+  name: string;
+  dimension?: string;
+  id?: number;
+}
+
+export interface IEpisode {
+  air_date: string;
+  episode: string;
+  created: string;
+  name: string;
+  id?: number;
 }
 
 export interface ICharacter {
@@ -14,52 +62,25 @@ export interface ICharacter {
   status: string;
   gender: string;
   species: string;
-  location: {
-    name: string;
-    id: number;
-    dimension: string;
-  };
+  location: ILocation;
 }
-export interface IDetails {
-  id: number;
-  name: string;
-  image: string;
-  status: string;
-  gender: string;
-  species: string;
-  location: {
-    name: string;
-    dimension: string;
-  };
-  episode: Array<{
-    air_date: string;
-    episode: string;
-    created: string;
-    name: string;
-  }>;
+
+export interface IDetails extends Omit<ICharacter, 'location'> {
+  location: Omit<ILocation, 'id'>;
+  episode: IEpisode[];
 }
-export enum GenderName {
-  male = 'Masculino',
-  female = 'Feminino',
-  unknown = 'Desconhecido',
+
+export interface IStateHome {
+  loadingPage: boolean;
+  listCharacteres: ICharacter[];
+  listDetails: IDetails;
+  pagination: IPagination;
 }
-export enum GenderColor {
-  male = 'grey-8',
-  female = 'purple',
-  unknown = 'green',
-}
-export enum StatusName {
-  dead = 'Morto',
-  alive = 'Vivo',
-  unknown = 'Desconhecido',
-}
-export enum StatusColor {
-  dead = 'red',
-  alive = 'green',
-  unknown = 'grey-8',
-}
-export enum SpeciesName {
-  alien = 'Alienígena',
-  human = 'Humano',
-  unknown = 'Desconhecido',
+
+export interface ICharacterFilter {
+  name?: string;
+  status?: keyof typeof StatusName;
+  species?: keyof typeof SpeciesName;
+  gender?: keyof typeof GenderName;
+  page?: number;
 }
